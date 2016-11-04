f1d890bc31d5fa9fefdf39f213c8d0bea0902078
Justin Lee <justin.lee@ubc.ca>
2016-11-04 10:32:54 -0700

    Added: tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 import http from 'http';
 import assert from 'assert';
 
-import '../lib/index.js';
+import functions from '../lib/index.js';
+
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
@@ -9,5 +10,55 @@ describe('Example Node Server', () => {
       assert.equal(200, res.statusCode);
       done();
     });
+  });
+});
+
+describe('Get Id', () => {
+  it('gets the IDs from an array of objects', done => {
+    var id = functions.getId([{
+      "id": 70111470,
+      "title": "Die Hard",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    }]);
+    var multipleID = functions.getId([
+    {
+      "id": 70111470,
+      "title": "Die Hard",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    },
+    {
+      "id": 654356453,
+      "title": "Bad Boys",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [5.0],
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    },
+    {
+      "id": 65432445,
+      "title": "The Chamber",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    },
+    {
+      "id": 675465,
+      "title": "Fracture",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [5.0],
+      "bookmark": [{ id: 432534, time: 65876586 }]
+    }
+  ]);
+    assert.equal(id, 70111470);
+    assert.deepEqual(multipleID, [70111470, 654356453, 65432445, 675465]);
+    done();
   });
 });
