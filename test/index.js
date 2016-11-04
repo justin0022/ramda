fe1c31a4c4cbd85fb22beef56ab4502a8e360e4b
Justin Lee <justin.lee@ubc.ca>
2016-11-04 15:53:48 -0700

    Added an array to play with

--- test/index.js
+++ test/index.js
@@ -3,6 +3,24 @@ import assert from 'assert';
 
 import functions from '../lib/index.js';
 
+var ponies = [
+    [
+        ['name', 'Fluttershy'],
+        ['image', 'http://tinyurl.com/gpbnlf6'],
+        ['description', 'Fluttershy is a female Pegasus pony and one of the main characters of My Little Pony Friendship is Magic.']
+    ],
+    [
+        ['name', 'Applejack'],
+        ['image', 'http://tinyurl.com/gkur8a6'],
+        ['description', 'Applejack is a female Earth pony and one of the main characters of My Little Pony Friendship is Magic.']
+    ],
+    [
+        ['name', 'Twilight Sparkle'],
+        ['image', 'http://tinyurl.com/hj877vs'],
+        ['description', 'Twilight Sparkle is the primary main character of My Little Pony Friendship is Magic.']
+    ]
+];
+
 var oneObject = [{
     "id": 70111470,
     "title": "Die Hard",
